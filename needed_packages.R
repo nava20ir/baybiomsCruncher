ebd525b7fa95781381b34deaf239040b550cbc26
Amirhossein Sakhteman <amirhossein.sakhteman@tum.de>
2025-05-28 16:08:18 +0200

    Edit needed_packages.R

--- needed_packages.R
+++ needed_packages.R
@@ -50,59 +50,6 @@ s2 <- c(
 lapply(s1, function(x) {
   if (x %in% installed.packages()[, 1])
     return()
-root@12fdc49d4f74:/home/shiny# cat install_dependencies.R
-install.packages("V8")
-install.packages("SparseM")
-# cran
-s1 <- c(
-  "survminer",
-  "survival",
-  "fastmatch",
-  "reshape2",
-  "beeswarm",
-  "grDevices",
-  "shinycssloaders",
-  "shinythemes",
-  "networkD3",
-  "httr",
-  "RColorBrewer",
-  "psych",
-  "stringr",
-  "shiny",
-  "shinydashboard",
-  "shinyWidgets",
-  "shinybusy",
-  "matrixStats",
-  "flatxml",
-  "excelR",
-  "shinyjs",
-  "shinyFiles",
-  "DT",
-  "plotly",
-  "openxlsx",
-  "yaml",
-  "curl",
-  "sortable",
-  "BiocManager",
-  "password",
-  "ggseqlogo",
-  "devtools",
-  "RSQLite",
-  "readr"
-  )
-
-# # BIOC
-s2 <- c(
-  "Biobase",
-  "fgsea",
-  "S4Vectors",
-  "SummarizedExperiment"
-  )
-
-#
-lapply(s1, function(x) {
-  if (x %in% installed.packages()[, 1])
-    return()
   install.packages(x)
 })
 #
