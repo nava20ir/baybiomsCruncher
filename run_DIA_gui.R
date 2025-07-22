a94fa2fca3a8fabb012fb78c8faa34059c2ef859
Amirhossein Sakhteman <amirhossein.sakhteman@tum.de>
2025-07-22 21:36:10 +0200

    Edit run_DIA_gui.R column ordering

--- run_DIA_gui.R
+++ run_DIA_gui.R
@@ -1,48 +1,52 @@
-
 library(DIAgui)
 
-
-
 mapping_expr_raw_file_2_name <- function(mapping_csv,dian_sourcec){
-  mapping_csv$name = gsub(mapping_csv$name,pattern = '.raw',replacement ='')
   
+  mapping_csv$name = gsub(mapping_csv$name,pattern = '.raw',replacement ='')
   mapping_dic <- as.character(mapping_csv$mapping)
-
   names(mapping_dic) <- as.character(mapping_csv$name)
   dian_sourcec$File.Name <- sapply(dian_sourcec$Run, function(x)mapping_dic[x])
   
 return(dian_sourcec)
-
+  
 }
 
 
-baybioms_report_process <- function(data, header.id = "Protein.Group", sample.id = "File.Name",
-                                    quantity.id = "Precursor.Normalised", secondary.id = "Precursor.Id",
+baybioms_report_process <- function(data, header.id = "Protein.Group", 
+                                    sample.id = "File.Name",
+                                    quantity.id = "Precursor.Normalised",
+                                    secondary.id = "Precursor.Id",
                                     id_to_add = c("Protein.Names", "First.Protein.Description", "Genes"),
-                                    qv = 0.01, pg.qv = 0.01, p.qv = 1, gg.qv = 1, quality = 0.8,
+                                    qv = 0.01,
+                                    pg.qv = 0.01,
+                                    p.qv = 1,
+                                    gg.qv = 1,
+                                    quality = 0.8,
                                     only_proteotypic = FALSE,
-                                    get_pep = TRUE, only_pepall = FALSE, get_Top3 = FALSE, get_iBAQ = TRUE,
-                                    fasta = NULL, species = NULL, peptide_length = c(5,36), mapping_dic = NULL,
+                                    get_pep = TRUE,
+                                    only_pepall = FALSE,
+                                    get_Top3 = FALSE,
+                                    get_iBAQ = TRUE,
+                                    fasta = NULL,
+                                    species = NULL,
+                                    peptide_length = c(5,36),
+                                    mapping_dic = NULL,
                                     format = c("xlsx", "csv", "txt")){
 
   
+  if (is.null(mapping_dic)) stop('no mapping data') 
+  if (!all(c('name','mapping') %in% colnames(mapping_csv))) stop('Two columns: raw and mapping should exist in mapping file')
   report_raw <- diann_load(data)  #load your report file
   
-  if (!is.null(mapping_dic)){
-     tryCatch({
-       report <- mapping_expr_raw_file_2_name(mapping_dic,report_raw)
-      
-       
-     }, warning = function(w) {
-       message("Warning: ", conditionMessage(w))
-      report = report_raw
-     })
-   }
- 
-  
+   tryCatch({
+     report <- mapping_expr_raw_file_2_name(mapping_dic,report_raw)
+   }, error = function(w) {
+     stop(conditionMessage(w))
+   })
 
-  brut <- report
+  if(any(is.na(report$File.Name ))) stop('something went wrong with mapping experiment to raw files pleases check the mapping csv')
   
+  brut <- report
   if(header.id == "Protein.Group" | header.id == "Genes"){
     if(only_proteotypic){
       report <- report[which(report[["Proteotypic"]] != 0), ]
@@ -62,43 +66,20 @@ baybioms_report_process <- function(data, header.id = "Protein.Group", sample.id
                                 median_normalization = FALSE,
                                 pdf_out = file.path(NULL))
    
- 
-    ### Get peptides counts from preprocess
-    # pc <- iq_report %>% dplyr::group_by(protein_list, sample_list) %>% dplyr::mutate("countpep" = length(unique(id)))
-    # pc <- unique(pc[,c("protein_list", "sample_list", "countpep")])
-    # pc <- tidyr::spread(pc, sample_list, countpep)
-    # pc[is.na(pc)] <- 0
-    # pc <- as.data.frame(pc)
-    # rownames(pc) <- pc$protein_list
-    # pc$protein_list <- NULL
-    # pc <- pc[order(rownames(pc)),]
-    # colnames(pc) <- paste0("pep_count_", colnames(pc))
-    # pc$peptides_counts_all <- unname(apply(pc, 1, max))
-    # pc <- pc[,c(ncol(pc), 1:(ncol(pc)-1))]
-  
-  
-
-
     iq_report <- iq::fast_MaxLFQ(iq_report) #return a list, check it, call element with '$'
     iq_report <- iq_report$estimate #estimate is the dataset
     iq_report <- as.data.frame(iq_report)
     iq_report = 2^ iq_report # anti log of intensity values
     iq_report <- iq_report[order(rownames(iq_report)),]
-    #iq_report$peptides_counts_all <- pc$peptides_counts_all
-    #iq_report <- cbind(iq_report, pc)
+
     
-  
-  
   ### Add gene names and other informations; reshape data frame
   nc <- ncol(iq_report)
   iq_report[[header.id]] <- rownames(iq_report)
   rownames(iq_report) <- 1:nrow(iq_report)
-  
   report <- report[(report[[header.id]] %in% iq_report[[header.id]]),]
   report <- report[order(report[[header.id]]),]
-  
   col_n <- colnames(report)
-  
   for(i in id_to_add){
     if(i %in% col_n){
       iq_report[[i]] <- unique(report[,c(header.id, i)])[[i]]
@@ -106,7 +87,6 @@ baybioms_report_process <- function(data, header.id = "Protein.Group", sample.id
   }
   
   iq_report <- iq_report[,c((nc+1):ncol(iq_report), 1:nc)]  # reorder columns
-
   d_seq <- getallseq(pr_id = iq_report$Protein.Group,
                        fasta_file = TRUE,
                        bank_name = fasta)
@@ -122,26 +102,27 @@ baybioms_report_process <- function(data, header.id = "Protein.Group", sample.id
                          method = "sum")
     
     
-
-    print(n_info)
-    print(n_cond)
-    print(colnames(brut))
-    print(which(colnames(brut) %in% mapping_dic$mapping))
-    
     brut <- get_iBAQ(brut, proteinDB = d_seq,
                      id_name = "Protein.Group",
                      #ecol = n_info:(n_cond+1),
-                     ecol = which(colnames(brut) %in% mapping_dic$mapping),
+                     ecol = which(colnames(brut) %in% mapping_csv$mapping),
                      peptideLength = peptide_length,
                      proteaseRegExp = getProtease("trypsin"),keep_original = FALSE,
                      log2_transformed = FALSE)
     
-   #brut <- brut[,-c(n_info:(n_cond+1))]
-    
+  colnames(brut) <- gsub(colnames(brut),pattern='iBAQ_',replacement='')
+  brut <- brut[,c(setdiff(colnames(brut),mapping_dic$mapping),mapping_dic$mapping)]
+  iq_report <- iq_report[,c(setdiff(colnames(iq_report),mapping_dic$mapping),mapping_dic$mapping)]
   return(list('ibaq'=brut,'max_lfq'=iq_report))
-  
 }
 
+
+
+dian_tsv  = '/Projects/002_Proteomics/P376_Lorenzo_and_Claassens_DnaAcos/P376_02/amir_dian_test/results.tsv'
+fasta_file = '/Projects/002_Proteomics/P376_Lorenzo_and_Claassens_DnaAcos/P376_02/amir_dian_test/dia_gui.fasta'
+mapping_csv = read.csv('/Projects/002_Proteomics/P376_Lorenzo_and_Claassens_DnaAcos/P376_02/amir_dian_test/mapping.csv')
+res = baybioms_report_process(dian_tsv,fasta = fasta_file,mapping_dic=mapping_csv)
+ibaq = res$ibaq ; maxLFQ = res$max_lfq
 
 
 
